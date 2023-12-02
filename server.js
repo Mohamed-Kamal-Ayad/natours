c6fcae785df12c94444547af364a53dbfdb9a127
Mohamed Kamal <mohamedkamalayad20@gmail.com>
2023-12-02 23:29:57 +0200

    handle SIGTERM

--- server.js
+++ server.js
@@ -36,3 +36,10 @@ process.on('unhandledRejection', err => {
     process.exit(1);
   });
 });
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM RECEIVED! SHUTTING DOWN...');
+  server.close(() => {
+    console.log('Process terminsated!');
+  });
+});
